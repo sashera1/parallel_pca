c6bea2fdfa19372b1fc3f5eb02a534cddcc28325
Alexander Arditi <ukk8zb@virginial.edu>
2025-12-23 15:56:34 -0500

    coded but not yet tested: setup for centerandscale, population of sm

--- centerAndScale.cu
+++ centerAndScale.cu
@@ -4,6 +4,24 @@
 #include "centerAndScale.h"
 
 __global__ void centerAndScaleKernel(int* inputMatrix, int* outputMatrix, int rowCount, int colCount){
+    //naive implementation:
+    //one thread per column
+    //block of 32 threads (1 warp)
+    //max columns = ~3000
+    extern __shared__ int column[]; //stored in row-major format; ie for block 0, 0-31 are col 0-31 of row 0, 32-63 are col 0-31 of row 1, etc
+    int sumOfVals = 0;
+    unsigned int threadIdGlobal = BlockIdx.x * BlockDim.x + threadIdx.x;
+    if (threadIdGlobal < colCount){
+        for (unsigned int row = 0; row < rowcount; ++row){
+            int val = inputMatrix[row*rowCount + threadIdGlobal];
+            sumOfVals+=val;
+            column[row*rowCount + threadIdx.x] = val;
+        }
+    }
+    //at this point, shared mem is populated with raw values
+
+    
+
 
 
 }
@@ -16,10 +34,24 @@ int* centerAndScaleWrapper(int* inputMatrixHost, int rowCount, int colCount, boo
     
     int *scaledMatrixHost = (int *) malloc(rowCount*colCount*sizeof(int));
 
-    int threadsNeeded = colCount; //one thread per column: may need more than this
+    unsigned int threadsPerBlock = 32;
+    unsigned int blockCount = (colCount + threadsPerBlock - 1) / threadsPerBlock;
 
+    unsigned int sharedMemoryPerBlock = threadsPerBlock * rowCount * sizeof(int);
 
+    int* inputMatrixDevice;
+    cudaMalloc((void**)&inputMatrixDevice, rowCount*colCount*sizeof(int));
+    cudaMemcpy(inputMatrixDevice,inputMatrixHost,rowCount*colCount*sizeof(int),cudaMemcpyHostToDevice);
 
+    int* scaledMatrixDevice;
+    cudaMalloc((void**)&scaledMatrixDevice, rowCount*colCount*sizeof(int));
+
+    centerAndScaleKernel<<blockCount,threadsPerBlock,sharedMemoryPerBlock>>(inputMatrixDevice,scaledMatrixDevice,rowCount,colCount);
+
+    cudaMemcpy(inputMatrixHost,inputMatrixDevice,rowCount*colCount*sizeof(int),cudaMemcpyDeviceToHost);
+
+    cudaFree(inputMatrixDevice);
+    cudaFree(scaledMatrixDevice);
 
     return scaledMatrixHost;
 }
