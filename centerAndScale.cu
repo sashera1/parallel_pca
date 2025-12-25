909d8c1b4a48271cb803df361ef8c0d0b61b02cf
sashera1 <ukk8zb@virginia.edu>
2025-12-25 15:50:27 -0500

    comments in kernel, changes to header datatype to match shift to float

--- centerAndScale.cu
+++ centerAndScale.cu
@@ -7,34 +7,43 @@ __global__ void centerAndScaleKernel(float* inputMatrix, float* outputMatrix, in
     //naive implementation:
     //one thread per column
     //block of 32 threads (1 warp)
-    //max columns = ~1500
+    //likley max rows = ~768 (assuming 96kb)
+    //absolute max rows = 792 (assuming 99kb)
     extern __shared__ float column[]; //stored in row-major format; ie for block 0, 0-31 are col 0-31 of row 0, 32-63 are col 0-31 of row 1, etc
     float mean = 0;
-    unsigned int threadIdGlobal = BlockIdx.x * BlockDim.x + threadIdx.x;
+    unsigned int threadIdGlobal = blockIdx.x * blockDim.x + threadIdx.x;
     if (threadIdGlobal < colCount){
         for (unsigned int row = 0; row < rowCount; ++row){
-            float val = inputMatrix[row*rowCount + threadIdGlobal];
+            float val = inputMatrix[row*colCount + threadIdGlobal];
             mean+=val;
-            column[row*rowCount + threadIdx.x] = val;
+            column[row*blockDim.x + threadIdx.x] = val;
         }
     
-    //at this point, shared mem is populated with raw values
+        //at this point, shared mem is populated with raw values
 
-    //calculate mean
+        //calculate mean
+        mean = mean / (float)rowCount;
+        float standardDevInv = 0;
 
-    //go thru and for each val
-    //accumulate difference square between val mean
-    //and subtract mean from val in sm
-    
+        //go thru and for each val
+        //accumulate difference square between val mean
+        //and subtract mean from val in sm
+        for (unsigned int row = 0; row < rowCount; ++row){
+            float val = column[row*blockDim.x + threadIdx.x];
+            float dif = (val-mean);
+            column[row*blockDim.x + threadIdx.x] = dif;
+            standardDevInv += dif * dif;
+        }
 
-
-    //once done, divide accumulation of squared differences by rowCount-1 to get standard d
-    //then go thru and dvidie each val by standard dev
+        standardDevInv = rsqrtf(standardDevInv / (float)rowCount); 
+        //consider: "When scaling, always add a tiny "epsilon" value inside your square root" 1e-5f
+        
+        //read shared mem values, multiply by standardDevInv, writeback to output matrix
+        for (unsigned int row=0; row < rowCount; ++row){
+            float val = column[row*blockDim.x + threadIdx.x];
+            outputMatrix[row*colCount + threadIdGlobal] = val * standardDevInv;
+        }
     }
-
-
-
-
 }
 
 float* centerAndScaleWrapper(float* inputMatrixHost, int rowCount, int colCount, bool debugMode){
@@ -42,24 +51,31 @@ float* centerAndScaleWrapper(float* inputMatrixHost, int rowCount, int colCount,
     if (debugMode){
         std::cout << "Entered centerAndScale file\n";
     }
+
+    size_t matrixSize = rowCount*colCount*sizeof(float)
     
-    float *scaledMatrixHost = (float *) malloc(rowCount*colCount*sizeof(float));
+    float *scaledMatrixHost = (float *) malloc(matrixSize);
 
     unsigned int threadsPerBlock = 32;
     unsigned int blockCount = (colCount + threadsPerBlock - 1) / threadsPerBlock;
 
     unsigned int sharedMemoryPerBlock = threadsPerBlock * rowCount * sizeof(float);
 
+    if (debugMode){
+        std::count << "Requesting " << sharedMemoryPerBlock / 1024.0f << "KB shared memory per block\n";
+    }
+
     float* inputMatrixDevice;
-    cudaMalloc((void**)&inputMatrixDevice, rowCount*colCount*sizeof(float));
-    cudaMemcpy(inputMatrixDevice,inputMatrixHost,rowCount*colCount*sizeof(float),cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&inputMatrixDevice, matrixSize);
+    cudaMemcpy(inputMatrixDevice,inputMatrixHost,matrixSize,cudaMemcpyHostToDevice);
 
     float* scaledMatrixDevice;
-    cudaMalloc((void**)&scaledMatrixDevice, rowCount*colCount*sizeof(float));
+    cudaMalloc((void**)&scaledMatrixDevice, matrixSize);
 
-    centerAndScaleKernel<<blockCount,threadsPerBlock,sharedMemoryPerBlock>>(inputMatrixDevice,scaledMatrixDevice,rowCount,colCount);
+    centerAndScaleKernel<<<blockCount,threadsPerBlock,sharedMemoryPerBlock>>>(
+        inputMatrixDevice,scaledMatrixDevice,rowCount,colCount);
 
-    cudaMemcpy(inputMatrixHost,inputMatrixDevice,rowCount*colCount*sizeof(float),cudaMemcpyDeviceToHost);
+    cudaMemcpy(scaledMatrixHost,scaledMatrixDevice,matrixSize,cudaMemcpyDeviceToHost);
 
     cudaFree(inputMatrixDevice);
     cudaFree(scaledMatrixDevice);
